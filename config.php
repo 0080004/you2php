1e5df8b4f807577c636b9e829c8ab85876a02ce1
0080004 <32374971+0080004@users.noreply.github.com>
2018-12-16 05:19:44 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyCf49QDgmMDiC3WqWKG8b-Zl-_-4M7K1c4');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
+define('SITE_NAME', '来自火星的视频');//网站名字
+define('TITLENAME', '火星视频');//网站名字
+define('EN2DEKEY', '3R~9tJsm');//一个随机字符串，加解密KEY
+define('EMAIL', 'michlsfejxga@yahoo.com');//邮箱
+?>
