2f30d4a308059eb5830df583ac79619cdd9c0abd
0080004 <32374971+0080004@users.noreply.github.com>
2018-12-16 06:39:23 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCf49QDgmMDiC3WqWKG8b-Zl-_-4M7K1c4');//这里改成您的api key
+define('APIKEY', 'AIzaSyAs7qswGA5pyIag6PtNyTNdpJTbBjn1y1Y');//这里改成您的api key
 define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', '来自火星的视频');//网站名字
 define('TITLENAME', '火星视频');//网站名字
